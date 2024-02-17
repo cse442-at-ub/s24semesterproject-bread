c4d31117c403e2d3e267d63345aa505080241f32
ShixinWu16 <shixinwu16@gmail.com>
2024-02-17 17:40:49 -0500

    Separated message.php
    Created a index.php file to handle message.php
    Separated the file so the link for message isnt message.php
    link for message is now /sendmessage

--- backend/sendmessage/message.php
+++ backend/sendmessage/message.php
@@ -17,7 +17,6 @@ function insertMessageIntoDatabase($message) {
         exit;
     }
 
-    
     $result = $conn->query("SHOW TABLES LIKE 'messages'");
     if ($result->num_rows == 0) {
         // Table doesn't exist, so create it
@@ -65,32 +64,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_SERVER['CONTENT_TYPE']) && 
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Send Message</title>
-    <script>
-        function sendMessage() {
-            var message = document.getElementById('message').value;
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "message.php", true);
-            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    // Parse JSON response
-                    var response = JSON.parse(xhr.responseText);
-                    alert(response.message);
-                }
-            };
-            xhr.send(JSON.stringify({message: message}));
-        }
-    </script>
-</head>
-<body>
-    <form onsubmit="sendMessage(); return false;">
-        <label for="message">Message:</label><br>
-        <input type="text" id="message" name="message" required><br>
-        <input type="submit" value="Send Message">
-    </form>
-</body>
-</html>
