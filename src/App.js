d05925201cb98850d75b39025f0d31f906bdbaec
ShixinWu16 <shixinwu16@gmail.com>
2024-02-17 19:40:50 -0500

    redid entire code
    made sure signup/login/home connects to one another

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUp from './webpages/signup/SignUp';
+import Login from './webpages/login/Login';
+import Home from './webpages/home/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
