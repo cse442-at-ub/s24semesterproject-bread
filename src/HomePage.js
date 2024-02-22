46ed6bbcb6c6d53d6f312c4cdfe453391b71ecc7
ShixinWu16 <shixinwu16@gmail.com>
2024-02-22 13:19:06 -0500

    Improved Security of previous backend code
    Code should not be weak to SQL Injection (Hopefully)
    removed all database login information and replaced it with a db_config.php file
    not added to Github for security reasons

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link to="/signin">
+                <button>Sign In</button>
+            </Link>
+            <Link to="/signup">
+                <button>Sign Up</button>
+            </Link>
+        </div>
+    );
+};
+
+export default HomePage;
