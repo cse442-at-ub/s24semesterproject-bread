9a98364a942b5ceed6506a62e9e0fb450bdda16c
ShixinWu16 <92799652+ShixinWu16@users.noreply.github.com>
2024-02-22 17:59:18 -0500

    Update db_config.php
    Removed login info

--- backend/db_config.php
+++ backend/db_config.php
@@ -1,6 +1,6 @@
 <?php
-    $servername = "oceanus.cse.buffalo.edu";
-    $username = "swu65";
-    $password = "50411773";
-    $dbname = "cse442_2024_spring_team_ac_db";
+    $servername = ""; // enter servername
+    $username = ""; // enter username
+    $password = ""; // enter password
+    $dbname = ""; // enter database name
 ?>
