e73116e68c06d817cce9cea1e176a0b9020deac7
ShixinWu16 <shixinwu16@gmail.com>
2024-02-22 16:39:14 -0500

    Fixed removed user
    remove user now removes user from both sessions and users

--- backend/removeuser/index.php
+++ backend/removeuser/index.php
@@ -1,15 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Delete Account</title>
+    <title>Test Page</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("#submit").click(function(e){
+                e.preventDefault();
+                var username = $("#username").val();
+                var sessionId = $("#sessionId").val();
+                var userID = $("#userID").val();
+                $.ajax({
+                    url: 'remove.php',
+                    type: 'post',
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    success: function (data) {
+                        $('#result').html(data.message);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        $('#result').html('Error: ' + textStatus + ' ' + errorThrown);
+                    },
+                    data: JSON.stringify({ "username": username, "sessionId": sessionId, "userID": userID })
+                });
+            });
+        });
+    </script>
 </head>
 <body>
-    <form action="remove.php" method="post">
+    <h1>Welcome to the Test Page</h1>
+    <p>This is a simple test page for the remove.php script.</p>
+    <form>
         <label for="username">Username:</label><br>
         <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
-        <input type="submit" value="Delete Account">
+        <label for="sessionId">Session ID:</label><br>
+        <input type="text" id="sessionId" name="sessionId"><br>
+        <label for="userID">User ID:</label><br>
+        <input type="number" id="userID" name="userID"><br>
+        <input type="submit" id="submit" value="Submit">
     </form>
+    <div id="result"></div>
 </body>
 </html>
