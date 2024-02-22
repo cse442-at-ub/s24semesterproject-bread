8cb2a5e00d2f0eb335841e73b87d9c875e528399
Reayoal <143447909+Reayoal@users.noreply.github.com>
2024-02-22 17:45:10 -0500

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+// Main.js
+
+import React from 'react';
+import './Main.css'; 
+import eyeLogo from './eye.png';
+
+function Main() {
+  return (
+<main className="container">
+<div className="title-container">
+        <h2 className="title">InSight</h2> <img className="logo" src={eyeLogo} alt="Logo" />
+      </div>
+  <hr className="separator" />
+  <div className="space"></div>
+  <h3 className="subtitle">Sign In</h3>
+  <form className="form">
+    <div className="input">
+      <label className="label">Email</label>
+      <input className="textfield" type="email" placeholder="Enter your email" />
+    </div>
+    <div className="input">
+      <label className="label">Password</label>
+      <input className="textfield" type="password" placeholder="Enter your password" />
+    </div>
+    <div className="forgot-password">
+      <a href="#" className="link"></a>
+    </div>
+    <div className="extra-space"></div>
+    <button className="primary" type="submit">Sign In</button>
+  </form>
+  <div className="signup-link">
+  <p className="link">New to Insight? <a href="https://www-student.cse.buffalo.edu/CSE442-542/2024-Spring/cse-442ac/Signup/" className="link">Sign Up</a></p>
+  </div>
+</main>
+  );
+}
+
+export default Main;
