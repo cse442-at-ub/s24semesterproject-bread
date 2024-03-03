ad572887c595cd021be0ce22df3855c623c17fde
Reayoal <yzhu53@buffalo.edu>
2024-03-03 14:23:58 -0500

    Connect signup frontend to backend API, add alert prompts
    This commit integrates the frontend signup form with the backend API by making the necessary API calls. Additionally, it adds functionality to display alert prompts when the user's input does not meet the required standards. This ensures that users are notified immediately about any errors or issues with their input.
    
    Reason for changes:
    - To provide a seamless connection between the frontend and backend for user registration
    - To enhance user experience by providing immediate feedback through alert prompts for invalid input

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,22 @@ import AccountSettings from './webpages/accountsettings/accountsettings';
 import Homepage from './webpages/homepage/homepage';
 import Professors from "./webpages/professorPage/professorPage";
 import ProfessorPage from './webpages/professorPage/professorPage' // Adjust the import path as necessary
-import QuizPage from './webpages/quizPage/QuizPage';
+//import QuizPage from './webpages/quizPage/QuizPage';
+import Review from './webpages/Review/Review';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/loginpage" element={<LogInPage />} />
+          <Route path="/signinpage" element={<LogInPage />} />
           <Route path="/signuppage" element={<SignUpPage />} />
           <Route path="/" element={<StartPage />} /> 
+          <Route path="/review" element={<Review />} /> 
           <Route path="/Homepage" element={<Homepage />} /> 
           <Route path="/accountsettings" element={<AccountSettings />} />
           <Route path="/professorPage" element={<ProfessorPage />} /> {/* Example static professor page */}
-          <Route path="/quizPage" element={<QuizPage />} />
-
+          {/* <Route path="/quizPage" element={<QuizPage />} /> */}
           <Route path="/professor/:name" element={<ProfessorPage />} /> {/* Dynamic professor page */}
         </Routes>
       </div>
--- src/webpages/Review/Review.jsx
+++ src/webpages/Review/Review.jsx
@@ -18,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+
       {screenWidth <= 540 ? <AlternateMain /> : <Main />}
     </div>
   );
