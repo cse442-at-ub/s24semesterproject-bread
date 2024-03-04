99e91e60a3e676d8a86cf6d60ed080b4dd406743
jiewen <jiewenhu@buffalo.edu>
2024-03-04 00:40:21 -0500

    Uploaded a dummy image for professors. Worked on the professor page, specifically incoporated a header which gives an over view of the professor, tried to make the rating stand out more to the user.

---
+++ src/webpages/professorPage/professorInfo.jsx
@@ -0,0 +1,11 @@
+const professorInfo = {
+    name: "John Doe",
+    department: "Department of Engineering and Applied Sciences",
+    rating: "4.8",
+    numberOfReviews: "120",
+    coursesTaught: ["CSE115", "CSE116", "CSE331"]
+};
+
+
+
+export default professorInfo;
