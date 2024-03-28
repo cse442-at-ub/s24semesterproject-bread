1cb88a04d795c18a887a6625b78e631c8a4e043c
jiewen <jiewenhu@buffalo.edu>
2024-03-28 01:15:50 -0400

    Worked on branch sprint3-132-frontend-saveButtonForProfessor, added the saved button for task card #132, however, the button has no functionality, it is going to be implemented later. THis button belongs on the professor madden card, so if a user wants to save this professor for future uses, it will be saved and displayed in the "saved" page which can be accesses through the menu bar, BUT once again this will be implemented later.

--- src/webpages/professorPage/professorPage.jsx
+++ src/webpages/professorPage/professorPage.jsx
@@ -3,12 +3,21 @@ import { useParams, useNavigate } from 'react-router-dom';
 import './ProfessorCard.css';
 import NavBar from '../navBar/NavBar';
 import defaultPic from "../../images/defaultPic.png";
+import savedIcon from "../../images/saved_icon.png";
+import savedIconColored from "../../images/saved_icon_colored.png";
+
 
 const ProfessorCard = () => {
     const navigate = useNavigate();
     const { name } = useParams();
     const [profname, setProfName] = useState('');
     const [profdepartment, setProfDepartment] = useState('');
+    const [isSaved, setIsSaved] = useState(false);
+
+    const toggleSave = () => {
+        setIsSaved(!isSaved);
+    };
+
 
     const [professorInfo, setProfessorInfo] = useState({
         name: '',
@@ -73,6 +82,12 @@ const ProfessorCard = () => {
         <div className='professor-main'>
             <NavBar/>
             <div className="professor-card">
+                <img
+                    src={isSaved ? savedIconColored : savedIcon}
+                    alt="Save Professor"
+                    className="saved-icon"
+                    onClick={toggleSave}
+                />
                 <img src={professorInfo.profilePicture || defaultPic} alt="Professor" className="professor-img" />
                 <div className="professor-info">
                     <h2>{professorInfo.name}</h2>
